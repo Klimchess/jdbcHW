82cfe6d753ac40f27aa146d780a3d8baca69e2ac
klimchess <vanyusha.1996@mail.ru>
2023-05-31 19:14:05 +0300

    initial commit

---
+++ src/main/java/EmployeeDao.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface EmployeeDao {
+    void add(Employee employee);
+    Employee getById(int id);
+    List<Employee> getAllEmployee();
+    void updateEmployee(int id, Employee employee);
+    void deleteEmployee(int id);
+}
